b05b37327ec746835fd50325c575e7b607cc2795
jon.graca@gmail.com <jon.graca@gmail.com>
2018-01-02 15:50:29 -0500

    Updating with WIMP Service

---
+++ src/app/wimp.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/expand';
+import 'rxjs/add/observable/empty';
+
+@Injectable()
+export class WimpService {
+
+    private baseUrl = 'http://localhost:8080/';
+
+    found = false;
+    extractData;
+    handleError;
+    apiUrl;
+
+    constructor (private http: Http) {}
+
+    getRecords(endpoint: string): Observable<any> {
+        const apiUrl = this.baseUrl + endpoint;
+        return this.http.get(apiUrl)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    getRecord(endpoint: string, id): Observable<object> {
+        const apiUrl = `${this.baseUrl}${endpoint}/${id}`;
+        return this.http.get(apiUrl)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    deleteRecord(endpoint: string, id: number): Observable<object> {
+        const apiUrl = `${this.baseUrl}${endpoint}/${id}`;
+        return this.http.delete(apiUrl)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    editRecord(endpoint: string, record: object, id: number): Observable<object> {
+        const apiUrl = `${this.baseUrl}${endpoint}/${id}`;
+        return this.http.put(apiUrl, record)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    addRecord(endpoint: string, record: object): Observable<any> {
+        const apiUrl = `${this.baseUrl}${endpoint}`;
+        console.log(apiUrl);
+        return this.http.post(apiUrl, record)
+            .map(this.extractData);
+    }
+}
